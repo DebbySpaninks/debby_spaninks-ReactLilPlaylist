e193c03ff475179772a3cfc5c8c014d6ebd30e7d
DebbySpaninks <dspaninks@gmail.com>
2021-03-10 11:49:22 +0100

    create map tree and new files

---
+++ react-playlist/src/container/SongOverview.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import SongForm from '../components/SongForm';
+import SongTable from '../components/table/SongTable';
+
+class SongOverview extends Component {
+
+    constructor() {
+        super()
+        this.state =
+        {
+            songs: []
+        }
+
+    }
+
+    addSong = (song) => {
+        // doe iets om de state aan te passen
+    }
+
+    render() {
+        return (
+            <div>
+                <SongForm addSong={this.addSong} />
+                <table style={{ width: "100%" }}>
+                    <tr className="song-header">
+                        <th className="song-row__item">Song</th>
+                        <th className="song-row__item">Artist</th>
+                        <th className="song-row__item">Genre</th>
+                        <th className="song-row__item">Rating</th>
+                    </tr>
+                </table>
+                <SongTable songs={this.state.songs} />
+            </div>
+        );
+    }
+}
+
+export default SongOverview;
