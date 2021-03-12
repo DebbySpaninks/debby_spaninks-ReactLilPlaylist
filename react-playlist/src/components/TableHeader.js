1c43073c555209d014154ce28d414bb2e41956ea
DebbySpaninks <dspaninks@gmail.com>
2021-03-12 13:33:58 +0100

    add steps for sort task

---
+++ react-playlist/src/components/TableHeader.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// function to display table headers
+function TableHeader(props) {
+
+// create function to sort songs 
+// sort on title a-z and z-a (capitalize??)
+// sort on artist a-z and z-a (capitalize??)
+// sort rating stars 1-5 and 5-1
+
+    return (
+        <thead>
+            <tr className="table-header">
+                {/* create sort button (in th) for title */}
+                <th>Title</th>
+
+                {/* create sort button (in th) for artist */}
+                <th>Artist</th>
+
+                <th>Genre</th>
+
+                {/* create sort button (in th) for rating stars */}
+                <th>Rating</th>
+            </tr>
+        </thead>
+    );
+}
+
+export default TableHeader;
