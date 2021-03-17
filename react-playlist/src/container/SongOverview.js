c6f9fe170f81033ec0d5984ef7affa7780602f95
DebbySpaninks <dspaninks@gmail.com>
2021-03-17 09:57:38 +0100

    es linter

--- react-playlist/src/container/SongOverview.js
+++ react-playlist/src/container/SongOverview.js
@@ -5,27 +5,9 @@ import SongTable from '../components/table/SongTable';
 // function that manage useState from songs 
 function SongOverview() {
     const [songs, setSongs] = useState([
-        {
-            id: 1,
-            title: "Onderweg",
-            artist: "Abel",
-            genre: "Nederpop",
-            rating: 4,
-        },
-        {
-            id: 2,
-            title: "Lose Yourself",
-            artist: "Eminem",
-            genre: "Hiphop Rap",
-            rating: 3,
-        },
-        {
-            id: 3,
-            title: "Domino",
-            artist: "Jessie J",
-            genre: "Pop",
-            rating: 4,
-        },
+        { id: 1, title: "Onderweg", artist: "Abel", genre: "Nederpop", rating: 4 },
+        { id: 2, title: "Lose Yourself", artist: "Eminem", genre: "Hiphop Rap", rating: 3 },
+        { id: 3, title: "Domino", artist: "Jessie J", genre: "Pop", rating: 4, }
     ]);
 
     // function that add songItems (useState, songs, setSongs)
@@ -33,7 +15,7 @@ function SongOverview() {
         let songItems = [...songs, song];
         setSongs(songItems);
     };
-    
+
     // function that delete song (useState, setSongs)
     const deleteSong = id => {
         let deletedSong = songs.filter(song => {
