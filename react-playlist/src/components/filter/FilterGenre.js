82954f1a7b92854d846d7e61ba6ba9292c6e7abc
DebbySpaninks <dspaninks@gmail.com>
2021-03-17 13:02:14 +0100

    change location input checkbox

--- react-playlist/src/components/filter/FilterGenre.js
+++ react-playlist/src/components/filter/FilterGenre.js
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // function to filter genre
 const FilterGenre = (props) => {
-    // function to target songs after click genre (checkbox)
-    const handleChangeEvent = e => {
-        switch (e.target.value) {
-            case 'pop':
-                document.write('this is the Pop genre');
-                // filterSongs('Pop');
-                break;
-            case 'nederpop':
-                document.write('this is the Nederpop genre');
-                // filterSongs('Nederpop');
-                break;
-            case 'hiprap':
-                document.write('this is the Hiphop Rap genre');
-                // filterSongs('Hiphop Rap');
-                break;
-            default:
-                document.write('There are no songs available that match the genre you have selected');
-                break;
-        }
-    };
+    const [checkedGenre, setCheckedGenre] = useState(false);
 
-    // add eventListener on checkbox
-    const addEventOnCheckbox = document.getElementsByName('checkbox');
-    addEventOnCheckbox.forEach(check => check.addEventListener('change', handleChangeEvent));
+    const handleChange = e => {
+        console.log("handleChange is working")
+    }
 
     return (
-        <div>
-            <h6> filter genre </h6>
-        </div>
+        <tr class="checkbox">
+            {/* create empty td's, now checkbox and labels right above Genre */}
+            <td class="empty-td"></td>
+            <td class="empty-td"></td>
+            <td>
+                <input
+                    type="checkbox"
+                    // id="genre-pop"
+                    name="pop"
+                    value="pop"
+                    checked={checkedGenre.value}
+                    onChange={handleChange} />
+                <label class="filter-label" for="genre-pop">Pop</label>
+
+                <input
+                    type="checkbox"
+                    // id="genre-nederpop"
+                    name="nederpop"
+                    value="nederpop"
+                    checked={checkedGenre.value}
+                    onChange={handleChange} />
+                <label class="filter-label" for="genre-nederpop">Nederpop</label>
+
+                <input
+                    type="checkbox"
+                    // id="genre-hiprap"
+                    name="hiprap"
+                    value="hiprap"
+                    checked={checkedGenre.value}
+                    onChange={handleChange} />
+                <label class="filter-label" for="genre-hiprap">Hiphop Rap</label>
+            </td>
+        </tr>
     )
 }
 
