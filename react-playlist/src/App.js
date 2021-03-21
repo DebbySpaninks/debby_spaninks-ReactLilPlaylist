d7edc91c94bbb3a56424c439af88a5bb1d35b308
DebbySpaninks <dspaninks@gmail.com>
2021-03-21 11:59:09 +0100

    router is working!

--- react-playlist/src/App.js
+++ react-playlist/src/App.js
@@ -1,52 +1,26 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import NavBar from './components/routing/NavBar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import About from './components/About';
 import SongOverview from './container/SongOverview';
 import './styles/App.css';
 
-// function to display Router, Header and SongOverview
+// function to display Routing, Header and SongOverview
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      {/* <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          {/* <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router> */}
-      <Header />
-      <SongOverview />
+      <Router>
+        <Header />
+        <Switch>
+          {/* If the current URL is /about, this route is rendered */}
+          <Route path="/about" component={About}></Route>
+          {/* Route with path="/" ALWAYS match the URL because all begin with /. This route must come at end */}
+          <Route path="/" component={SongOverview}></Route>
+        </Switch>
+      </Router>
     </div>
+
   );
-}
-
-// function Home() {
-//   return <h2>Home</h2>;
-// }
-
-// function About() {
-//   return <h2>About</h2>;
-// }
+};
 
 export default App;
--- react-playlist/src/components/Header.js
+++ react-playlist/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
     return(
         <div className="header">
             <h1>React Playlist</h1>
+            <Link to="/"> Home </Link>
+            <Link to="/about"> About </Link>
+            <p></p>
         </div>
     );
 }
