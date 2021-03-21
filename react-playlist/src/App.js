066d13a568e6d5b3f200f16e6d12e455fe68da6c
DebbySpaninks <dspaninks@gmail.com>
2021-03-21 08:07:32 +0100

    add routing and remove list style

--- react-playlist/src/App.js
+++ react-playlist/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import SongOverview from './container/SongOverview';
 import './styles/App.css';
@@ -7,10 +8,43 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
       <Header />
       <SongOverview />
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
 }
 
 export default App;
