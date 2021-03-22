784dbbd6d034c0ce651ac696797ae61b7594353d
DebbySpaninks <dspaninks@gmail.com>
2021-03-22 11:38:29 +0100

    change names and delete prev testCodes

--- react-playlist/src/components/filter/FilterSelect.js
+++ react-playlist/src/components/filter/FilterSelect.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 
 // function to filter genre                naam van functie nog aanpassen en van component!!
 function FilterSelect() {
-    const [selectGenre, setSelectGenre] = useState('');
-    const [selectRating, setSelectRating] = useState('');
+    const [selectedGenre, setSelectedGenre] = useState('');
+    const [selectedRating, setSelectedRating] = useState('');
 
     // handleChange or handleSubmit to return the array with filtered items
     // in SongOverview staat functie om te filteren maar is nog niet meegegeven aan FilterSelect
@@ -17,10 +17,10 @@ function FilterSelect() {
             <td className="white-td">
                 <select
                     name="genre"
-                    value={selectGenre}
+                    value={selectedGenre}
                     className="select-filter"
                     // onChange={handleSelectGenre}
-                    onChange={e => setSelectGenre(e.target.value)}
+                    onChange={e => setSelectedGenre(e.target.value)}
                     >
                     <option value="">Filter genre</option>
                     <option value="pop">Pop</option>
@@ -31,10 +31,10 @@ function FilterSelect() {
             <td className="white-td">
                 <select
                     name="rating"
-                    value={selectRating}
+                    value={selectedRating}
                     className="select-filter" 
                     // onChange={handleSelectRating}
-                    onChange={e => setSelectRating(e.target.value)}
+                    onChange={e => setSelectedRating(e.target.value)}
                 >
                     <option value="">Filter rating</option>
                     <option value="1">1</option>
@@ -47,40 +47,5 @@ function FilterSelect() {
         </tr>
     );
 };
-
-
-    // // functie to select genre and log it
-    // const handleSelectGenre = e => {
-    //     switch (e.target.value) {
-    //         case 'pop':
-    //         case 'nederpop':
-    //         case 'hiphoprap':
-    //             console.log('genre', {}, 'is geselecteerd');
-    //             // filterGenre({ genre: selectGenre, id: id });
-    //             break;
-    //         default:
-    //             console.log('no genres are selected');
-    //             break;
-    //     };
-    //     setSelectGenre();
-    // };
-
-    // // function to select rating and log it
-    // const handleSelectRating = e => {
-    //     switch (e.target.value) {
-    //         case '1':
-    //         case '2':
-    //         case '3':
-    //         case '4':
-    //         case '5':
-    //             console.log('rating' , {}, 'is geselecteerd');
-    //             // filterRating('');
-    //             break;
-    //         default:
-    //             console.log('no ratings are selected');
-    //             break;
-    //     };
-    //     setSelectRating();
-    // };
 
 export default FilterSelect;
